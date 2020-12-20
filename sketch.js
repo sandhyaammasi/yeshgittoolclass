3d572be549d98b98cfe24774bf009dffaaf703f7
Sandhya Ammasi <sandhyaammasi@gmail.com>
2020-12-21 01:01:50 +0530

    created one more log & added a description

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(200,300,30,PI);
     bird = new Bird(100,100);
 
 }
@@ -59,7 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+log6.display();
     bird.display();
     platform.display();
 }
